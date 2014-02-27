07991e2a1766343024a5c9ab30e15b1b3e687a3f
Jan Nanista <jan.nanista@gooddata.com>
2014-02-27 13:35:01 +0100

    Changed project's invite() method to accept string login

--- test/project_test.js
+++ test/project_test.js
@@ -18,18 +18,20 @@ describe('Project', function() {
         this.api = new API(config);
         this.user = new User(this.api);
         this.project = new Project(this.api, this.user);
-        
+
         sinon.spy(https, 'request');
-        
-        this.user.login(config).then(function() {
+
+        this.user.login(config).done(function() {
             done();
+        }, function(error) {
+            throw error;
         });
     });
-    
+
     afterEach(function() {
         https.request.restore();
     });
-    
+
     it('should create project', function(done) {
         this.project.create({
             title: 'Some project title',
@@ -37,11 +39,11 @@ describe('Project', function() {
         }).done(function(data) {
             expect(data).to.have.property('content');
             expect(data).to.have.property('meta');
-            
+
             done();
         });
     });
-    
+
     context('when created', function() {
         before(function(done) {
             this.createdProject = new Project(this.api, this.user);
@@ -52,39 +54,48 @@ describe('Project', function() {
                 done();
             });
         });
-        
+
+        it('should load project', function(done) {
+            this.createdProject.load().done(function(data) {
+                expect(data).to.have.property('content');
+                expect(data).to.have.property('meta');
+
+                done();
+            });
+        });
+
         it('should list roles', function(done) {
             this.createdProject.listRoles().done(function(roles) {
                 expect(roles).to.be.an('array');
-                
+
                 done();
             });
         });
-        
+
         it('should get role by name', function(done) {
             this.createdProject.getRoleByName('Admin').done(function(role) {
                 expect(role).to.be.a(Project.Role);
-                
+
                 done();
             });
         });
-        
+
         it('should invite user', function(done) {
             var userData = {
                 username: config.username.replace('@', '+' + Date.now() + '@'),
                 password: config.password
             };
-            
+
             var user = new User(this.api);
             user.register(userData).then(function() {
                 return user.activate(userData);
             }).then(function() {
-                return this.createdProject.invite(user, 'Admin', userData);
+                return this.createdProject.invite(userData.username, 'Admin', userData);
             }.bind(this)).done(function() {
                 done();
             });
         });
-        
+
         it('should delete project', function(done) {
             this.createdProject.delete().done(function() {
                 done();
